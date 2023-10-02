1fb41e762e6cfaa82d23a3bd0edd1accad2d4e76
Xushnudbek <86800818+XushnudbekTursunboyev@users.noreply.github.com>
2023-10-02 14:09:13 +0500

    contactItem update

--- app/src/main/java/uz/gita/testcleanafb6/presentation/components/ContactItem.kt
+++ app/src/main/java/uz/gita/testcleanafb6/presentation/components/ContactItem.kt
@@ -1,6 +1,8 @@
 package uz.gita.testcleanafb6.presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,56 +33,72 @@ fun ContactItem(
     onClickSettings: (ContactParam) -> Unit,
     onClickEdit: (ContactParam) -> Unit
 ) {
-    Row(
-        modifier = Modifier
-            .clickable { onClickEdit(data) }
-            .padding(8.dp)
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 12.dp),
-        verticalAlignment = Alignment.CenterVertically
+
+    Box(modifier = Modifier
+        .clickable { onClickEdit(data) }
+        .fillMaxWidth()
+        .padding(horizontal = 16.dp)
+        .padding(top = 12.dp)
     ) {
-        Icon(painter = painterResource(id = R.drawable.ic_user), contentDescription = "profile", modifier = Modifier.width(40.dp).height(40.dp).clip(shape = CircleShape))
-        Spacer(modifier = Modifier.size(12.dp))
 
-        Column(
-            modifier = Modifier
-                .weight(0.7f, true)
-        ) {
-
-            Text(
-                text = data.name,
-                style = MaterialTheme.typography.bodyLarge
-            )
-
-            Spacer(modifier = Modifier.size(4.dp))
-
-            Text(text = data.phone)
-
-        }
-
-        Column(modifier = Modifier.weight(0.3f)) {
-
-        }
-
-        IconButton(
-            onClick = {
-                onClickSettings(data)
-            },
-            content = {
+        Column {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 Icon(
-                    painter = painterResource(id = R.drawable.ic_settings),
-                    contentDescription = "Settings",
-                    modifier = Modifier
-                        .width(24.dp)
-                        .height(24.dp)
+                    painter = painterResource(id = R.drawable.ic_user), contentDescription = "profile", modifier = Modifier
+                        .width(40.dp)
+                        .height(40.dp)
+                        .clip(shape = CircleShape)
                 )
-            },
-        )
+                Spacer(modifier = Modifier.size(12.dp))
+
+                Column(
+                    modifier = Modifier
+                        .weight(0.7f, true)
+                ) {
+
+                    Text(
+                        text = data.name,
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+
+                    Spacer(modifier = Modifier.size(4.dp))
+
+                    Text(text = data.phone)
+
+                }
+
+                Column(modifier = Modifier.weight(0.3f)) {
+
+                }
+
+                IconButton(
+                    onClick = {
+                        onClickSettings(data)
+                    },
+                    content = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_settings),
+                            contentDescription = "Settings",
+                            modifier = Modifier
+                                .width(24.dp)
+                                .height(24.dp)
+                        )
+                    },
+                )
+            }
+
+            Spacer(modifier = Modifier.size(12.dp))
+            Spacer(modifier = Modifier.fillMaxWidth().height(1.dp).background(color = Color.Cyan))
+        }
     }
+
+
 }
 
 @Composable
 @Preview(showBackground = true)
-fun ContactItemPreview(){
-    ContactItem(data = ContactParam(1, "", ""), onClickSettings = {ContactParam(1, "", "")}, onClickEdit = {ContactParam(1, "", "")})
+fun ContactItemPreview() {
+    ContactItem(data = ContactParam(1, "", ""), onClickSettings = { ContactParam(1, "", "") }, onClickEdit = { ContactParam(1, "", "") })
 }
