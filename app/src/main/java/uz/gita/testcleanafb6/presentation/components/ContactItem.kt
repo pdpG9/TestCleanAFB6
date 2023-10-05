c6f1da8de2f9f06658707b3380ff1d63cba8c3f6
Xushnudbek <86800818+XushnudbekTursunboyev@users.noreply.github.com>
2023-10-05 16:12:26 +0500

    contactItem update dataClass

--- app/src/main/java/uz/gita/testcleanafb6/presentation/components/ContactItem.kt
+++ app/src/main/java/uz/gita/testcleanafb6/presentation/components/ContactItem.kt
@@ -1,9 +1,11 @@
 package uz.gita.testcleanafb6.presentation.components
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,27 +13,37 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.PopupProperties
 import uz.gita.testcleanafb6.R
 import uz.gita.testcleanafb6.data.network.response.AddContactData
+import uz.gita.testcleanafb6.domain.models.ContactParam
 
 @Composable
 fun ContactItem(
-    data: AddContactData,
-    onClickSettings: (AddContactData) -> Unit,
-    onClickEdit: (AddContactData) -> Unit
+    data: ContactParam,
+    onClickSettings: (ContactParam) -> Unit,
+    onClickEdit: (ContactParam) -> Unit
 ) {
 
     Box(modifier = Modifier
@@ -49,7 +61,7 @@ fun ContactItem(
                     painter = painterResource(id = R.drawable.ic_user), contentDescription = "profile", modifier = Modifier
                         .width(40.dp)
                         .height(40.dp)
-                        .clip(shape = CircleShape) ,
+                        .clip(shape = CircleShape),
                     tint = Color(0xffBDBDBD)
                 )
                 Spacer(modifier = Modifier.size(12.dp))
@@ -60,13 +72,13 @@ fun ContactItem(
                 ) {
 
                     Text(
-                        text = data.name,
+                        text = data.data.name,
                         style = MaterialTheme.typography.bodyLarge
                     )
 
                     Spacer(modifier = Modifier.size(4.dp))
 
-                    Text(text = data.phone)
+                    Text(text = data.data.phone)
 
                 }
 
@@ -84,7 +96,7 @@ fun ContactItem(
                             contentDescription = "Settings",
                             modifier = Modifier
                                 .width(24.dp)
-                                .height(24.dp) ,
+                                .height(24.dp),
                             tint = Color(0xffBDBDBD)
                         )
                     },
@@ -92,15 +104,17 @@ fun ContactItem(
             }
 
             Spacer(modifier = Modifier.size(12.dp))
-            Spacer(modifier = Modifier.fillMaxWidth().height(1.dp).background(color = Color.Cyan))
+            Spacer(modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(color = Color.Cyan))
         }
     }
-
-
 }
+
 
 @Composable
 @Preview(showBackground = true)
 fun ContactItemPreview() {
-    ContactItem(data = AddContactData(1, "", ""), onClickSettings = { AddContactData(1, "", "") }, onClickEdit = { AddContactData(1, "", "") })
+    ContactItem(data = ContactParam(false, AddContactData(1, "","")), onClickSettings = { ContactParam(false, AddContactData(1, "","")) }, onClickEdit = { ContactParam(false, AddContactData(1, "","")) })
 }
