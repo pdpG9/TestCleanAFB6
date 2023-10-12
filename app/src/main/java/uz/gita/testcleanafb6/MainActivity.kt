2a02a9e9497f9967d6fe8d408a6bca88ee4241c4
Shukrullo Zokirov <axborotxavfsizligi@gmail.com>
2023-10-12 16:16:13 +0500

    MainActivityga qoshildi

--- app/src/main/java/uz/gita/testcleanafb6/MainActivity.kt
+++ app/src/main/java/uz/gita/testcleanafb6/MainActivity.kt
@@ -30,6 +30,7 @@ class MainActivity : ComponentActivity() {
                             .collect()
                     }
                     CurrentScreen()
+                    ///yangi code bu
                 }
             }
         }
